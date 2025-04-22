64544d0c216423dfc4f6b442a246da87db121919
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-22 13:13:58 +0530

    update

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const cors=require("cors");
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+
+
+
+
+dotenv.config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+//app.use('/api/captcha',captchaRoutes);
+
+
+const port=7000;
+app.listen(port,()=>{
+    console.log("server starting at port:",port);
+})
+
+
+
+
+// mongoose.connect("mongodb+srv://deepumelkani123:Kk8c5KST9Ib3J3FP@cluster0.aujdhik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{useNewUrlParser:true,useUnifiedTopology:true})
+// .then(()=>{app.listen(2500,()=>{console.log("server running on port 2500")})})
+// .catch(err=>console.log(err));
+//Kk8c5KST9Ib3J3FP
+
+const connectDB=async ()=>{
+
+try{
+    mongoose.connect("mongodb+srv://deepumelkani123:Kk8c5KST9Ib3J3FP@cluster0.aujdhik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+console.log("Database connect successfully")
+}
+catch (error){
+console.error("Error connecting to database",error)
+}
+
+
+}
+connectDB();
