a10485d9a096e903381ae3fb6bf62da44ee4ed3b
Divyanshu Amdola <divyanshuamdola01@gmail.com>
2025-04-22 17:12:58 +0530

    updated

--- preprocessor-service/preprocessors/image_preprocess.py
+++ preprocessor-service/preprocessors/image_preprocess.py
@@ -5,23 +5,24 @@ import io
 from PIL import Image
 
 def preprocess_image(base64_str):
-    img_data=base64.b64decode(base64_str)
-    image=Image.open(io.BytesIO(img_data)).covert('RGB')
-    img=np.array(image)
+    # Decode the base64 string
+    img_data = base64.b64decode(base64_str)
+    image = Image.open(io.BytesIO(img_data)).convert('RGB')
+    img = np.array(image)
 
-    gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    _, thresh= cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
+    _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    contours,_=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.THRESH_BINARY_INV)
-
-    blobs=[]
+    blobs = []
     for cnt in contours:
-        x,y,w,h=cv2.boundingReact(cnt)
-        char=thresh[y:y+h,x:x+w]
-        resized=cv2.resize(char,(28,28))
-        _, buffer = cv2.imencode('.png',resized)
+        x, y, w, h = cv2.boundingRect(cnt)
+        char = thresh[y:y+h, x:x+w]
+        resized = cv2.resize(char, (28, 28))
+
+        _, buffer = cv2.imencode('.png', resized)
         blobs.append(base64.b64encode(buffer).decode('utf-8'))
 
-        return blobs
+    return blobs
--- preprocessor-service/preprocessors/text_preprocess.py
+++ preprocessor-service/preprocessors/text_preprocess.py
@@ -5,21 +5,22 @@ import io
 from PIL import Image
 
 def preprocess_text(base64_str):
-    img_data=base64.b64decode(base64_str)
-    image=Image.open(io.BytesIO(img_data)).convert('L')
-    img=np.array(image)
+    img_data = base64.b64decode(base64_str)
+    image = Image.open(io.BytesIO(img_data)).convert('L')
+    img = np.array(image)
 
-    _,thresh=cv2.threshold(img,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    _, thresh = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    coords=np.column_stack(np.where(thresh>0))
-    angle=cv2.minAreaRect(coords)[-1]
-    if angle<-45:
-        angle= -(90+angle)
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
     else:
-        angle= -angle
-    (h,w)=thresh.shape[:2]
-    M=cv2.getRotationMatrix2D((w//2,h//2),angle,1.0)
-    deskewed=cv2.warpAffine(thresh,M,(w,h),flags=cv2.INTER_CUBIC)
+        angle = -angle
 
-    _,buffer=cv2.imencode('.png',deskewed)
-    return base64.b64decode(buffer).decode('utf-8')
+    (h, w) = thresh.shape[:2]
+    M = cv2.getRotationMatrix2D((w // 2, h // 2), angle, 1.0)
+    deskewed = cv2.warpAffine(thresh, M, (w, h), flags=cv2.INTER_CUBIC)
+
+    _, buffer = cv2.imencode('.png', deskewed)
+    return base64.b64encode(buffer).decode('utf-8')
