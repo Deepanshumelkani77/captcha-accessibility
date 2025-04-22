49c574abef4e5d9d48328908e56d4ce1ed962aa1
Divyanshu Amdola <divyanshuamdola01@gmail.com>
2025-04-22 16:32:38 +0530

    trained

---
+++ preprocessor-service/preprocessors/image_preprocess.py
@@ -0,0 +1,27 @@
+import base64
+import numpy as np
+import cv2
+import io
+from PIL import Image
+
+def preprocess_image(base64_str):
+    img_data=base64.b64decode(base64_str)
+    image=Image.open(io.BytesIO(img_data)).covert('RGB')
+    img=np.array(image)
+
+    gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    _, thresh= cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV)
+
+
+
+    contours,_=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.THRESH_BINARY_INV)
+
+    blobs=[]
+    for cnt in contours:
+        x,y,w,h=cv2.boundingReact(cnt)
+        char=thresh[y:y+h,x:x+w]
+        resized=cv2.resize(char,(28,28))
+        _, buffer = cv2.imencode('.png',resized)
+        blobs.append(base64.b64encode(buffer).decode('utf-8'))
+
+        return blobs
