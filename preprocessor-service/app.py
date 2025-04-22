fc04d06a33c53320975acbdf4aa9143fb2625917
Divyanshu Amdola <divyanshuamdola01@gmail.com>
2025-04-22 14:43:52 +0530

    app.py

--- preprocessor-service/app.py
+++ preprocessor-service/app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI,Request
 from preprocessor import image_preprocess,text_preprocess,audio_preprocess
 
-app=FastAPI();
+app=FastAPI()
 
-
+@app.post("/preprocess/{type}")
+async def preprocess(type:str,request:Request):
+    body=await request.json()
+    data=body["data"]
+    if type=="image":
+        return image_preprocess(data)
+    elif type=="text":
+        return text_preprocess(data)
+    elif type=="audio":
+        return audio_preprocess(data)
